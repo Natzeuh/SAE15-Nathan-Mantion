b6d28cde0f71cbb39059c23f5cb8778f2feb429e
Natzeuh <psg97130@gmail.com>
2023-01-16 15:51:32 +0100

    fonction getPark

--- acquis.py
+++ acquis.py
@@ -1,11 +1,10 @@
 import requests
 import time
 
-def getPark(idPark:str):
+def getPark(idPark:str,path="."):
 	response=requests.get(f"https://data.montpellier3m.fr/sites/default/files/ressources/{idPark}.xml")
-	file=open(f"{idPark}_{int(time.time())}.xml","w+")
+	file=open(f"{path}/{idPark}_{int(time.time())}.xml","w+", encoding="UTF-8")
 	file.write(response.text)
-	file.close
+	file.close()
 
 
-getPark("Bouchard")
