7f93c92d0da52168dbc84b1b68ee7e714b366ef3
Natzeuh <psg97130@gmail.com>
2023-01-16 15:57:22 +0100

    fonction getCycle + ajout de return pour path file

--- acquis.py
+++ acquis.py
@@ -6,5 +6,13 @@ def getPark(idPark:str,path="."):
 	file=open(f"{path}/{idPark}_{int(time.time())}.xml","w+", encoding="UTF-8")
 	file.write(response.text)
 	file.close()
+	return file.name
 
+def getCycle(path="."):
+	response=requests.get("https://montpellier-fr-smoove.klervi.net/gbfs/en/station_status.json")
+	file=open(f"{path}/veloMag_{int(time.time())}.json","w+", encoding="UTF-8")
+	file.write(response.text)
+	file.close()
+	return file.name
 
+getCycle()
