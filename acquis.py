62bbc42a9f8ea3f207fdcf70f6afd8851e20118d
Natzeuh <psg97130@gmail.com>
2023-01-16 15:47:02 +0100

    Premier jet de libraire d'acquisition

--- acquis.py
+++ acquis.py
@@ -1,2 +1,11 @@
 import requests
 import time
+
+def getPark(idPark:str):
+	response=requests.get(f"https://data.montpellier3m.fr/sites/default/files/ressources/{idPark}.xml")
+	file=open(f"{idPark}_{int(time.time())}.xml","w+")
+	file.write(response.text)
+	file.close
+
+
+getPark("Bouchard")
