c9372127525e3748d7f8eb36bcbbde0f99ba0ee3
Natzeuh <psg97130@gmail.com>
2023-01-18 10:16:27 +0100

    stockage ébut

---
+++ stockage.py
@@ -0,0 +1,14 @@
+import sqlite3 as sq
+from acquis import *
+from parkNStations import *
+
+def savePark(park:parking):
+	#connection base
+	#curseur
+	#construction query
+	#exécution de la query
+	#sauvegarde
+	#fin fonction
+
+def saveVelo(stat:velo):
+	
